5e0f1f2f8671f02ac5853f784d7015c981277d9d
Administrator <admin@example.com>
2025-01-09 11:13:31 +0700

    ...

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,12 @@
-$fluentbit_uri = "https://drive.usercontent.google.com/download?id=1blK-swTWSMV-gBtUyZLc4kH3pVes8unU`&export=download`&authuser=0`&confirm=t`&uuid=3fb2c611-4787-4b5d-8fb3-e376f76b4386`&at=AIrpjvO2RuXGfEML8JPEY40wVNdq:1736395742906"
-$fluentbit_service = "fluentbit"
-#  Get-ChildItem -Path "$API_FOLDER_PATH\API_HN212" -Recurse -Force |
-
 sc.exe stop $fluentbit_service
 sc.exe delete $fluentbit_service
 Remove-Item -Path "C:\fluentbit.zip" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "C:\fluentbit" -Recurse -Force -ErrorAction SilentlyContinue
+
+$fluentbit_uri = "https://drive.usercontent.google.com/download?id=1blK-swTWSMV-gBtUyZLc4kH3pVes8unU`&export=download`&authuser=0`&confirm=t`&uuid=3fb2c611-4787-4b5d-8fb3-e376f76b4386`&at=AIrpjvO2RuXGfEML8JPEY40wVNdq:1736395742906"
+$fluentbit_service = "fluentbit"
+#  Get-ChildItem -Path "$API_FOLDER_PATH\API_HN212" -Recurse -Force |
+
 
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $fluentbit_uri -OutFile "C:\fluentbit.zip"
